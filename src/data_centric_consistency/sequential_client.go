7c88303371efab24323a0047bdbf35a373ec74cf
rtarun9 <rtarun0913@gmail.com>
2023-06-09 05:16:59 +0530

    Fixed issues with RPC sequential data centric model

--- src/data_centric_consistency/sequential_client.go
+++ src/data_centric_consistency/sequential_client.go
@@ -43,6 +43,13 @@ type ValueRetrievalRequest struct {
 	Process  Process
 }
 
+type MultipleValueRetrievalRequest struct {
+	Variable1 byte
+	Variable2 byte
+
+	Process Process
+}
+
 func Process1(wg *sync.WaitGroup) {
 	client, err := rpc.Dial("tcp", "localhost:1234")
 
@@ -203,22 +210,188 @@ func Process3(wg *sync.WaitGroup) {
 	client.Call("Server.GetValue", xRetrievalRequest, &p3)
 	client.Call("Server.GetValue", yRetrievalRequest, &p3)
 
-	fmt.Printf("[From Processes with ID :: %d at local time :: %d] X = %d, Y = %d.\n", 3, p3.CurrentTime, p3.Y.Value, p3.Z.Value)
+	fmt.Printf("[From Processes with ID :: %d at local time :: %d] X = %d, Y = %d.\n", 3, p3.CurrentTime, p3.X.Value, p3.Y.Value)
+
+	wg.Done()
+}
+
+// Sequentially consistent version of the above program.
+
+func Process1Consistent(wg *sync.WaitGroup) {
+	client, err := rpc.Dial("tcp", "localhost:1234")
+
+	if err != nil {
+		log.Fatal("Connection error: ", err)
+	}
+
+	p1 := Process{
+		PID: 1,
+		X: DataItem{
+			Value:            0,
+			LastUpdationTime: 0,
+		},
+		Y: DataItem{
+			Value:            0,
+			LastUpdationTime: 0,
+		},
+		Z: DataItem{
+			Value:            0,
+			LastUpdationTime: 0,
+		},
+	}
+
+	// Update value of x.
+	p1.X.Value = 1
+	p1.CurrentTime = 1
+
+	updationRequest := UpdationRequest{
+		Variable:         'X',
+		Value:            1,
+		LastUpdationTime: p1.CurrentTime,
+		Process:          p1,
+	}
+
+	client.Call("Server.UpdateValue", updationRequest, &p1)
+	fmt.Printf("[(Update) From Processes with ID :: %d at local time :: %d] Y = %d, Z = %d.\n", 1, p1.CurrentTime, p1.Y.Value, p1.Z.Value)
+
+	// Get value of Y and Z
+	yzRetrievalRequest := MultipleValueRetrievalRequest{
+		Variable1: 'Y',
+		Variable2: 'Z',
+		Process:   p1,
+	}
+
+	client.Call("Server.GetValueMultiple", yzRetrievalRequest, &p1)
+
+	fmt.Printf("[(Retrieval) From Processes with ID :: %d at local time :: %d] Y = %d, Z = %d.\n", 1, p1.CurrentTime, p1.Y.Value, p1.Z.Value)
+
+	wg.Done()
+}
+
+func Process2Consistent(wg *sync.WaitGroup) {
+	client, err := rpc.Dial("tcp", "localhost:1234")
+
+	if err != nil {
+		log.Fatal("Connection error: ", err)
+	}
+
+	p2 := Process{
+		PID: 2,
+		X: DataItem{
+			Value:            0,
+			LastUpdationTime: 0,
+		},
+		Y: DataItem{
+			Value:            0,
+			LastUpdationTime: 0,
+		},
+		Z: DataItem{
+			Value:            0,
+			LastUpdationTime: 0,
+		},
+	}
+
+	// Update value of y.
+	p2.Y.Value = 1
+	p2.CurrentTime = 1
+
+	updationRequest := UpdationRequest{
+		Variable:         'Y',
+		Value:            1,
+		LastUpdationTime: p2.CurrentTime,
+		Process:          p2,
+	}
+
+	client.Call("Server.UpdateValue", updationRequest, &p2)
+	fmt.Printf("[(Update) From Processes with ID :: %d at local time :: %d] X = %d, Z = %d.\n", 2, p2.CurrentTime, p2.X.Value, p2.Z.Value)
+
+	// Get value of X and Z
+	xzRetrievalRequest := MultipleValueRetrievalRequest{
+		Variable1: 'X',
+		Variable2: 'Z',
+		Process:   p2,
+	}
+
+	client.Call("Server.GetValueMultiple", xzRetrievalRequest, &p2)
+
+	fmt.Printf("[(Retrieve) From Processes with ID :: %d at local time :: %d] X = %d, Z = %d.\n", 2, p2.CurrentTime, p2.X.Value, p2.Z.Value)
+
+	wg.Done()
+}
+
+func Process3Consistent(wg *sync.WaitGroup) {
+	client, err := rpc.Dial("tcp", "localhost:1234")
+
+	if err != nil {
+		log.Fatal("Connection error: ", err)
+	}
+
+	p3 := Process{
+		PID: 3,
+		X: DataItem{
+			Value:            0,
+			LastUpdationTime: 0,
+		},
+		Y: DataItem{
+			Value:            0,
+			LastUpdationTime: 0,
+		},
+		Z: DataItem{
+			Value:            0,
+			LastUpdationTime: 0,
+		},
+	}
+
+	// Update value of z.
+	p3.Z.Value = 1
+	p3.CurrentTime = 1
+
+	updationRequest := UpdationRequest{
+		Variable:         'Z',
+		Value:            1,
+		LastUpdationTime: p3.CurrentTime,
+		Process:          p3,
+	}
+
+	client.Call("Server.UpdateValue", updationRequest, &p3)
+	fmt.Printf("[(Update) From Processes with ID :: %d at local time :: %d] X = %d, Y = %d.\n", 3, p3.CurrentTime, p3.X.Value, p3.Y.Value)
+
+	// Get value of X and Y
+	xyRetrievalRequest := MultipleValueRetrievalRequest{
+		Variable1: 'X',
+		Variable2: 'Y',
+		Process:   p3,
+	}
+
+	client.Call("Server.GetValueMultiple", xyRetrievalRequest, &p3)
+
+	fmt.Printf("[(Retrieve) From Processes with ID :: %d at local time :: %d] X = %d, Y = %d.\n", 3, p3.CurrentTime, p3.X.Value, p3.Y.Value)
 
 	wg.Done()
 }
 
 func main() {
-	// Running the simulation for 4 times.
 	for i := 0; i < 1; i++ {
-		fmt.Println("\nSimulation :: ", i)
 
 		wg := sync.WaitGroup{}
+		if false {
+			fmt.Println("\nSimulation :: Sequentially Inconsistent Data", i)
+			wg.Add(3)
+
+			go Process1(&wg)
+			go Process2(&wg)
+			go Process3(&wg)
+
+			wg.Wait()
+		}
+
+		fmt.Println("\nSimulation :: Sequentially Consistent Data", i)
+
 		wg.Add(3)
 
-		go Process1(&wg)
-		go Process2(&wg)
-		go Process3(&wg)
+		go Process1Consistent(&wg)
+		go Process2Consistent(&wg)
+		go Process3Consistent(&wg)
 
 		wg.Wait()
 	}
--- src/data_centric_consistency/sequential_server.go
+++ src/data_centric_consistency/sequential_server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net"
 	"net/rpc"
 	"sync"
@@ -46,24 +47,36 @@ type ValueRetrievalRequest struct {
 	Process  Process
 }
 
+type MultipleValueRetrievalRequest struct {
+	Variable1 byte
+	Variable2 byte
+
+	Process Process
+}
+
 var datastore = Datastore{}
 
 func (server *Server) UpdateValue(request UpdationRequest, response *Process) error {
 	log.Println("Updation Request from :: ", request.Process.PID)
 
-	for !datastore.Mutex.TryLock() {
-		request.Process.CurrentTime += 1
-	}
+	request.Process.CurrentTime += 1
+	datastore.Mutex.Lock()
+
+	request.Process.CurrentTime = uint32(math.Max(float64(request.Process.CurrentTime), float64(datastore.X.LastUpdationTime)))
+	request.Process.CurrentTime = uint32(math.Max(float64(request.Process.CurrentTime), float64(datastore.Y.LastUpdationTime)))
+	request.Process.CurrentTime = uint32(math.Max(float64(request.Process.CurrentTime), float64(datastore.Z.LastUpdationTime)))
+
+	request.Process.CurrentTime += 1
 
 	if request.Variable == 'X' {
 		datastore.X.Value = request.Value
-		datastore.X.LastUpdationTime = request.Process.CurrentTime + 1
+		datastore.X.LastUpdationTime = request.Process.CurrentTime
 	} else if request.Variable == 'Y' {
 		datastore.Y.Value = request.Value
-		datastore.Y.LastUpdationTime = request.Process.CurrentTime + 1
+		datastore.Y.LastUpdationTime = request.Process.CurrentTime
 	} else if request.Variable == 'Z' {
 		datastore.Z.Value = request.Value
-		datastore.Z.LastUpdationTime = request.Process.CurrentTime + 1
+		datastore.Z.LastUpdationTime = request.Process.CurrentTime
 	}
 
 	*response = request.Process
@@ -79,25 +92,76 @@ func (server *Server) UpdateValue(request UpdationRequest, response *Process) er
 func (server *Server) GetValue(request ValueRetrievalRequest, response *Process) error {
 	log.Println("GetValue Request from :: ", request.Process.PID)
 
-	for !datastore.Mutex.TryLock() {
-		request.Process.CurrentTime += 1
-	}
+	request.Process.CurrentTime += 1
+	datastore.Mutex.Lock()
 
+	request.Process.CurrentTime = uint32(math.Max(float64(request.Process.CurrentTime), float64(datastore.X.LastUpdationTime)))
+	request.Process.CurrentTime = uint32(math.Max(float64(request.Process.CurrentTime), float64(datastore.Y.LastUpdationTime)))
+	request.Process.CurrentTime = uint32(math.Max(float64(request.Process.CurrentTime), float64(datastore.Z.LastUpdationTime)))
+
+	request.Process.CurrentTime += 1
 	if request.Variable == 'X' {
-		if request.Process.CurrentTime < datastore.X.LastUpdationTime {
+		if request.Process.CurrentTime >= datastore.X.LastUpdationTime {
 			request.Process.X = datastore.X
 		}
 	} else if request.Variable == 'Y' {
-		if request.Process.CurrentTime < datastore.Y.LastUpdationTime {
+		if request.Process.CurrentTime >= datastore.Y.LastUpdationTime {
 			request.Process.Y = datastore.Y
 		}
 	} else if request.Variable == 'Z' {
-		if request.Process.CurrentTime < datastore.Z.LastUpdationTime {
+		if request.Process.CurrentTime >= datastore.Z.LastUpdationTime {
 			request.Process.Z = datastore.Z
 		}
 	}
 
+	*response = request.Process
+
+	fmt.Printf("State of global datastore: \n\tX value and update time : %d %d\n\tY value and update time : %d %d\n\tZ Value and update time : %d %d\n",
+		datastore.X.Value, datastore.X.LastUpdationTime, datastore.Y.Value, datastore.Y.LastUpdationTime, datastore.Z.Value, datastore.Z.LastUpdationTime)
+	datastore.Mutex.Unlock()
+
+	return nil
+}
+
+func (server *Server) GetValueMultiple(request MultipleValueRetrievalRequest, response *Process) error {
+	log.Println("GetMultipleValue Request from :: ", request.Process.PID)
+
 	request.Process.CurrentTime += 1
+	datastore.Mutex.Lock()
+
+	request.Process.CurrentTime = uint32(math.Max(float64(request.Process.CurrentTime), float64(datastore.X.LastUpdationTime)))
+	request.Process.CurrentTime = uint32(math.Max(float64(request.Process.CurrentTime), float64(datastore.Y.LastUpdationTime)))
+	request.Process.CurrentTime = uint32(math.Max(float64(request.Process.CurrentTime), float64(datastore.Z.LastUpdationTime)))
+
+	request.Process.CurrentTime += 1
+
+	if request.Variable1 == 'X' {
+		if request.Process.CurrentTime >= datastore.X.LastUpdationTime {
+			request.Process.X = datastore.X
+		}
+	} else if request.Variable1 == 'Y' {
+		if request.Process.CurrentTime >= datastore.Y.LastUpdationTime {
+			request.Process.Y = datastore.Y
+		}
+	} else if request.Variable1 == 'Z' {
+		if request.Process.CurrentTime >= datastore.Z.LastUpdationTime {
+			request.Process.Z = datastore.Z
+		}
+	}
+
+	if request.Variable2 == 'X' {
+		if request.Process.CurrentTime >= datastore.X.LastUpdationTime {
+			request.Process.X = datastore.X
+		}
+	} else if request.Variable2 == 'Y' {
+		if request.Process.CurrentTime >= datastore.Y.LastUpdationTime {
+			request.Process.Y = datastore.Y
+		}
+	} else if request.Variable2 == 'Z' {
+		if request.Process.CurrentTime >= datastore.Z.LastUpdationTime {
+			request.Process.Z = datastore.Z
+		}
+	}
 
 	*response = request.Process
 
